e51c922907ac8a305dcdab23b43e9c9e533d1f8b
Anchit Makkar <social.mobikul@webkul.com>
2026-03-12 15:12:18 +0530

    Revert "Update Example app to use version 1.0.7"
    This reverts commit 70303810e42cec713d1c02694e3f9564e00ac2e7.

--- Example/app/build.gradle.kts
+++ Example/app/build.gradle.kts
@@ -58,7 +58,7 @@ dependencies {
     implementation("androidx.multidex:multidex:2.0.1")
     // BagistoNative Android SDK via JitPack
     // Check available versions: https://jitpack.io/#SocialMobikul/BagistoNative_Android/
-    implementation("com.github.SocialMobikul:BagistoNative_Android:1.0.7")
+    implementation("com.github.SocialMobikul:BagistoNative_Android:1.0.6")
 
     // AndroidX Core - matching SDK versions
     implementation("androidx.core:core-ktx:1.15.0")
